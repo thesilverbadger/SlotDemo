2349d9a72d80ad06807481f5d9816a80cc5a48a7
Tom Wire <tom.wire@poweredbypie.co.uk>
2016-05-16 14:20:31 +0100

    Add data project

---
+++ SlotDemo.Api/Api/PeopleController.cs
@@ -0,0 +1,19 @@
+﻿using SlotDemo.Data.Models;
+using SlotDemo.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SlotDemo.Api.Api
+{
+    public class PeopleController : ApiController
+    {
+        public List<Person> Get()
+        {
+            return new PersonRepository().GetAll();
+        }
+    }
+}
---
+++ SlotDemo.Data/Repositories/PersonRepository.cs
@@ -0,0 +1,31 @@
+﻿using SlotDemo.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlotDemo.Data.Repositories
+{
+    public class PersonRepository
+    {
+        public List<Person> GetAll()
+        {
+            return new List<Person>()
+            {
+                new Person()
+                {
+                    PersonId = 1,
+                    Firstname = "Joe",
+                    Lastname = "Bloggs"
+                },
+                new Person()
+                {
+                    PersonId = 2,
+                    Firstname = "Joanne",
+                    Lastname = "Smith"
+                }
+            };
+        }
+    }
+}
